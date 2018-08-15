feeb341720e2a6c042efc01068848df48536db59
Noak <noak.rosengren@gmail.com>
2018-08-15 13:56:08 +0200

    end of lecture 58

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -3,12 +3,13 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends Component{
+class SearchBar extends Component{
   constructor(props){
     super(props);
 
     this.state = {term: ''};
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
   onInputChange(event){
     console.log(event.target.value);
@@ -18,6 +19,8 @@ export default class SearchBar extends Component{
     event.preventDefault();
 
     //we need to go fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
   }
   render(){
     return(
@@ -34,3 +37,9 @@ export default class SearchBar extends Component{
     )
   }
 }
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({fetchWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
