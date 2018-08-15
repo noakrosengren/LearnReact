b0255a1aae22cf4f80fbf3d86911367bd15411ad
Noak <noak.rosengren@gmail.com>
2018-08-15 11:14:00 +0200

    end of lecture 54

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -11,9 +11,14 @@ export default class SearchBar extends Component{
     console.log(event.target.value);
     this.setState({term: event.target.value});
   }
+  onFormSubmit(event){
+    event.preventDefault();
+
+    //we need to go fetch weather data
+  }
   render(){
     return(
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a 5 day forecast in your favourite cities"
           className="form-control"
