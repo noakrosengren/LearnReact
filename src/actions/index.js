f65d54aa77cbee0d60e682f02d6184dcc023c3d8
Noak <noak.rosengren@gmail.com>
2018-08-15 13:40:58 +0200

    lecture58 3min

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY ='210e5c96dfc568f2d0a8002055a05dbd';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+    return{
+      type: FETCH_WEATHER,
+      payload: request
+    };
+}
